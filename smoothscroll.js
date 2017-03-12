da7a3b0de3d46ce41c00ea3b85fb0bd33074f849
Golam Rabbi <mdrabbi.hossain9@gmail.com>
2017-03-13 03:02:02 +0600

    second update
    second update commit

--- smoothscroll.js
+++ smoothscroll.js
@@ -12,3 +12,17 @@ $(document).ready(function(){
     });
   });
 });
+
+
+jQuery('a[href^="#"]').on('click', function(event) {
+
+    var target = jQuery(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        jQuery('html, body').stop().animate({
+            scrollTop: target.offset().top + 100
+        }, 1000);
+    }
+
+});
